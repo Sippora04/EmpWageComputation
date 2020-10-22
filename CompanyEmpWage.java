d8a02d40ebe5d1283c3e9204544729c7cb18b319
Sippora04 <sippora.toppo95@gmail.com>
2020-10-23 02:37:21 +0530

    Added: Total EmpWage by Company

---
+++ CompanyEmpWage.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+class CompanyEmpWage
+{
+        public final String company;
+        public final int empRatePerHour;
+        public final int workingDaysInMonth;
+        public final int maximunWorkHours;
+        public int totalEmpWage;
+        int totalSalary;
+        public ArrayList<Integer> dailyWage = new ArrayList<>();
+        public CompanyEmpWage( String company, int empRatePerHour, int workingDaysInMonth, int maximunWorkHours )
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.workingDaysInMonth = workingDaysInMonth;
+            this.maximunWorkHours = maximunWorkHours;
+        }
+
+    public void getTotalWage(int totalSalary )
+    {
+        this.totalSalary = totalSalary;
+    }
+    public void storeDailyWage(ArrayList<Integer> dailyWage )
+    {
+        this.dailyWage = dailyWage;
+    }   
+}
--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,28 +1,98 @@
-public class EmpWageComputation {
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
+import java.util.*;
+import java.util.Map.Entry;
+public class EmpWageComputation implements EmpWageInterface
+{   
+   int empHours = 0;
+   private Map<String, Integer> companyWageMap;
+   private final ArrayList<CompanyEmpWage> companyArray;
+   private int numofCompany=0;
+   
+   public EmpWageComputation()
+   {
+    companyArray = new ArrayList<>();
+    companyWageMap = new HashMap<>();
+   }
+   
+   public void addCompanyEmployee(String company, int empRatePerHour, int workingDaysInMonth, int maximunWorkHours)
+    {
+        CompanyEmpWage comp = new CompanyEmpWage( company, empRatePerHour, workingDaysInMonth, maximunWorkHours);
+        companyArray.add(comp);
+        numofCompany++;
+    }
 
-	public static void main(String[] args) {
-	        	    
-	        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-	        
-	        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-	        	totalWorkingDays++;
-	        	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-	        	switch (empCheck){
-	        		case IS_PART_TIME: empHrs = 4;
-	        					   		break;
-	        		case IS_FULL_TIME: empHrs = 8;
-		        							break;
-	        		default: empHrs = 0;
-	        	}
-	        	totalEmpHrs += empHrs;
-	        	System.out.println("Day " + totalWorkingDays + " Emp Wage of " + empHrs + " hr");
-	        }
-	        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-	        System.out.println("Total Emp Wage is " + totalEmpWage);
+   public int getWorkingHours(int empCheck )
+   {
+      switch (empCheck)
+      {
+         case IS_EMP_FULL_TIME:  empHours = 8 ;
+                                 break ;
+         case IS_EMP_PART_TIME:  empHours = 4 ;
+                                 break ;
+         default: empHours=0  ;
+      }
+      return empHours;
+   }
+
+   public void getCalculationDailyWage()
+   {    
+      for (int i=0; i< numofCompany; i++)
+      { 
+        getMonthlyWage(companyArray.get(i));
+	System.out.println(" Company: " + companyArray.get(i).company + " :  Employee Wage : " + companyArray.get(i).totalSalary);
+        int oldWage = 0;
+        System.out.println("Day     DailyWage       Total Wage");
+	for (int j=0; j < companyArray.get(i).dailyWage.size(); j++ )
+        {
+            System.out.println("Day: " + j +"       "+(companyArray.get(i).dailyWage.get(j)-oldWage)+"          "+companyArray.get(i).dailyWage.get(j));
+            oldWage = companyArray.get(i).dailyWage.get(j);
 	}
+      } 
+   }
+   
+   public void wagePerCompany()
+    {
+        Set<Entry<String, Integer>> set= companyWageMap.entrySet();
+        Iterator itr=set.iterator();
+        while(itr.hasNext())
+        {
+            Map.Entry entry=(Map.Entry)itr.next();
+            System.out.println(entry.getKey()+":"+entry.getValue());
+        }
+    }
+   
+   public int getMonthlyWage( CompanyEmpWage company )
+   {  
+      int totalEmpHours = 0, totalSalary = 0, totalWorkingDays = 0;
+      ArrayList<Integer> dailyWageArray = new ArrayList<>();
+      while (totalEmpHours < company.maximunWorkHours && totalWorkingDays < company.workingDaysInMonth )
+      {
+         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+         empHours = getWorkingHours(empCheck );
+         int dailyWage = 0;
+         dailyWage = company.empRatePerHour * empHours;
+         totalSalary = totalSalary + dailyWage;
+         dailyWageArray.add(totalSalary);
+         totalEmpHours += empHours;
+         ++totalWorkingDays;
+         
+        }
+         if( totalEmpHours > company.maximunWorkHours )
+         {
+            company.getTotalWage(totalSalary);
+         }   
+         company.storeDailyWage(dailyWageArray);
+         company.getTotalWage(totalSalary);
+         companyWageMap.put(company.company, totalSalary );
+       return 0;
+    }
+
+   public static void main(String args[])
+   {
+      System.out.println("\nWelcome to Employee Wage Computation/Problem\n");
+      EmpWageComputation employeeWage = new EmpWageComputation();
+      employeeWage.addCompanyEmployee("D Mart", 20, 4, 30 );
+      employeeWage.addCompanyEmployee("BigBazaar", 15, 5, 25 );
+      employeeWage.addCompanyEmployee("City Style", 8, 5 ,10 );
+      employeeWage.getCalculationDailyWage();
+   }
 }
